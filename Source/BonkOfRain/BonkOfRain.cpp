ff68b8dc0029ba7dbd291c4d7f975ea59efc332d
defoltbest <defoltbest@mail.ru>
2026-03-03 11:52:01 +0100

    гue5.7 init

---
+++ Source/BonkOfRain/BonkOfRain.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BonkOfRain.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BonkOfRain, "BonkOfRain" );
